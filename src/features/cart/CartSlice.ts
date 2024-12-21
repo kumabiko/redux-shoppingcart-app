186bc37fc16a137cc37b50b1e126967b435354fe
kumabiko <nousbear.dev@gmail.com>
2024-12-21 11:35:35 +0900

    init

---
+++ src/features/cart/CartSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit"
+import cartItems from "../../cartItems"
+
+const initialState = {
+  cartItems: cartItems,
+  amount: 0,
+  total: 0,
+}
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {},
+})
+
+export default cartSlice.reducer
